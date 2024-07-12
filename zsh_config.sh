0c9c647b9341195995e3806f0eca20afc68b12b1
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-07-11 21:03:07 -0300

    changed chmod command to match repo name

--- zsh_config.sh
+++ zsh_config.sh
@@ -4,8 +4,9 @@
 sudo apt update
 sudo apt install -y zsh
 
-chmod +x ~/FreshUbuntu/configure.sh
-chmod +x ~/FreshUbuntu/retrieve_projects.sh
+chmod +x ~/linuxtools/configure.sh
+chmod +x ~/linuxtools/retrieve_ssh.sh
+chmod +x ~/liinuxtools/retrieve_projects.sh
 
 # Change default shell to Zsh
 sudo chsh -s $(which zsh) $USER
