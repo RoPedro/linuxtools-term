6cd5bc9b72f71b4b680677e414c42a310db79338
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-11-05 11:36:07 -0300

    Tar now correctly extracts to lazygit

--- configure.sh
+++ configure.sh
@@ -56,7 +56,7 @@ clone_repositories() {
         echo "Installing lazygit"
         LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
         curl -Lo ~/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-        tar xf ~/lazygit.tar.gz ~/lazygit
+        tar xf ~/lazygit.tar.gz lazygit
         sudo install lazygit /usr/local/bin
 
         # Cleans the trash
