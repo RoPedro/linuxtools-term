6a1421422878baddcd1ae6dce134d551691ade5d
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-12-09 18:56:52 -0300

    removing unused retrieve scripts

--- zsh_config.sh
+++ zsh_config.sh
@@ -5,8 +5,6 @@ sudo apt update
 sudo apt install -y zsh
 
 chmod +x ~/linuxtools/configure.sh
-chmod +x ~/linuxtools/retrieve_ssh.sh
-chmod +x ~/liinuxtools/retrieve_projects.sh
 
 # Change default shell to Zsh
 sudo chsh -s $(which zsh) $USER
